24dc936dec343561c23915a13c168edf6e559acd
MichielAtMove <mvanwessem@move.com>
2014-12-11 17:17:32 -0800

    Find channels from AddListener calls

--- GetChannelNames/Program.cs
+++ GetChannelNames/Program.cs
@@ -16,41 +16,33 @@ namespace GetChannelNames
     {
         private static void Main(string[] args)
         {
-            var channels = GetChannels();
+            var channels1 = GetChannelsFromAddTrigger();
+            
+            var channels2 = GetChannelsFromAddListener();
 
-            var s = String.Format("public enum Channel = \n{{  {0}\n}};",
-                String.Join(", \n",
-                    from channel in channels orderby channel.Key select String.Format("\t{0}", channel.Key)));
+            var allnames = (from channel in channels1 select channel.Key).Union(from channel in channels2 select channel.Key).Distinct();
+
+            var s = FormatAsEnum(allnames);
 
             Trace.WriteLine(s);
 
 
-            /*
-            foreach (var channel in from channel in channels
-                                    orderby channel.Key 
-                                    select channel)
-            Trace.WriteLine(String.Format("{0}\tcount: {1}", channel.Key, channel.Value.Count));
-             */
         }
 
-        private static AutoDictionary<string, List<string>> GetChannels()
+        private static string FormatAsEnum(IEnumerable<string> channels)
         {
-            var document = new XmlDocument();
-            document.Load("../../../AddTrigger.xml");
-            var n = document.SelectNodes("/model/node[position()=2]/node/node/*");
+            return String.Format("public enum Channel = \n{{  {0}\n}};",
+                String.Join(", \n",
+                    from channel in channels orderby channel select String.Format("\t{0}", channel)));
+        }
 
-            var statements = from node in n.Cast<XmlNode>() select node.Attributes["column0"].Value;
-
-            // (21,27) signUpFormConnections.AddTrigger("SIGN_UP_SUCCESS", WidgetEventConnector.Events.Message, "USER_SIGNED_UP");
-
+        private static AutoDictionary<string, List<string>> GetChannelsFromAddTrigger()
+        {
             var pattern =
                 new Regex(
                     "\\([0-9]+,[0-9]+\\) (?:[a-zA-Z_][a-zA-Z0-9_$]*\\s*.\\s*)+AddTrigger\\s*\\(\\s*\"(?<eventname>[^\"]*)\"\\s*,\\s*[^,]+\\s*,\\s*\"(?<channelname>[^\"]*)\"");
 
-            var matches = statements.Select(statement => pattern.Match(statement)).ToArray();
-
-            if (matches.Any(match => !match.Success))
-                throw new ApplicationException();
+            var matches = Matches(pattern, "../../../AddTrigger.xml", "/model/node[position()=2]/node/node/*");
 
             var dictionary = new AutoDictionary<string, List<string>>();
 
@@ -62,6 +54,52 @@ namespace GetChannelNames
                 dictionary[channelname].Add(eventname);
             }
             return dictionary;
+        }
+
+        private static AutoDictionary<string, List<string>> GetChannelsFromAddListener()
+        {
+            var pattern =
+                new Regex(
+                    "\\([0-9]+,[0-9]+\\) (?:[a-zA-Z_][a-zA-Z0-9_$]*\\s*.\\s*)+AddListener\\s*\\(\\s*\"(?<channelname>[^\"]*)\"\\s*,\\s*\"(?<method>[^\"]*)\"");
+
+            Trace.WriteLine(pattern.ToString());
+
+            var matches = Matches(pattern, "../../../AddListener.xml", "/model/node/node/node/node/*");
+
+            var dictionary = new AutoDictionary<string, List<string>>();
+
+            foreach (var match in matches)
+            {
+                var methodname = match.Groups["methodname"].ToString();
+                var channelname = match.Groups["channelname"].ToString();
+
+                dictionary[channelname].Add(methodname);
+            }
+            return dictionary;
+        }
+
+        private static IEnumerable<Match> Matches(Regex pattern, string filename, string modelNodeNodeNodeNode)
+        {
+            var statementsFromResharperOutputDocument = GetStatementsFromResharperOutputDocument(filename, modelNodeNodeNodeNode);
+
+            var matches =
+                statementsFromResharperOutputDocument
+                    .Select(statement => pattern.Match(statement))
+                    .ToArray();
+            
+            return matches.Where(match => match.Success);
+        }
+
+        private static IEnumerable<string> GetStatementsFromResharperOutputDocument(string filename, string modelNodeNodeNodeNode)
+        {
+            var document = new XmlDocument();
+            document.Load(filename);
+            var n = document.SelectNodes(modelNodeNodeNodeNode);
+
+            if (n == null)
+                return null;
+
+            return from node in n.Cast<XmlNode>() where node.Attributes != null select node.Attributes["column0"].Value;
         }
     }
 }
